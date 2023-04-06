13b6e80ed0e0a63ff953be8dd62814fb5dc2981b
htw1120 <harrywynn1120@gmail.com>
2023-04-06 10:22:13 -0600

    Initial Commit

---
+++ modules/blobber.py
@@ -0,0 +1,48 @@
+# Harrison Wynn
+# CS1400 - M01
+# Assignment 9
+
+import math
+import time
+
+
+class Blobber:
+    def __init__(self, name, color, radius, height):
+        self.__radius = radius
+        self.__height = height
+        self.__color = color
+        self.__name = name
+        self.__time = time.time()
+        self.__volume = (math.pi * pow(radius, 2) * self.__height)
+
+    def feedBlobber(self, food):
+        if food >= 0:
+            self.__radius += food
+
+    def blobberSpeak(self):
+        vitals, blobberOK = self.vitalsOK()
+        return ("My name is " + self.__name.capitalize() + ", and I am " + self.__color.lower()) + "\n" + "My current happiness level is " + str(format(vitals, ".2%"))
+
+    def vitalsOK(self):
+        while True:
+            volume1 = math.pi * pow(self.__radius - ((time.time() - self.__time) * .0002), 2) * self.__height
+            vitals = volume1 / self.__volume
+            if .9 < vitals < 1.1:
+                return vitals, True
+            else:
+                return vitals, False
+
+    def setColor(self, color):
+        if type(color) == str:
+            self.__color = color
+
+    def setName(self, name):
+        if type(name) == str:
+            self.__name = name
+
+    def getColor(self):
+        return self.__color
+
+    def getName(self):
+        return self.__name
+    
